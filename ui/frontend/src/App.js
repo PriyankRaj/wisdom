7f98da3e932f12db49686f4a4e77f256b0363bc6
PriyankRaj <priyank@hams.co.in>
2025-01-05 10:42:42 +0530

    Update App.js

--- ui/frontend/src/App.js
+++ ui/frontend/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import axios from 'axios';
 import './styles/styles.css';
 import ContentTable from "./components/ContentTable/ContentTable";
+import Analysis from "./components/Analysis/Analysis";
 
 const App = () => {
     const [data, setData] = useState([]);
@@ -10,25 +12,22 @@ const App = () => {
     const fetchData = async () => {
       try {
         const response = await axios.get('http://ec2-3-110-148-174.ap-south-1.compute.amazonaws.com:5000/api/data');
+        //const response = await axios.get('http://ec2-3-110-148-174.ap-south-1.compute.amazonaws.com:5000/api/data');
         setData(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
-
     fetchData();
   }, []);
 
   return (
-      <div className="container">
-
-          <div className="layout">
-              <div>
-                  <ContentTable data={data}/>
-              </div>
-          </div>
-      </div>
-
+        <Router>
+            <Routes>
+                <Route path="/" element={<ContentTable data={data}/>} />
+                <Route path="/analysis" element={<Analysis/>} />
+            </Routes>
+        </Router>
   );
 };
 
